2df2a94868b86ecbaa4770dd72955e31d735cb1f
s4fv4n <42152758+s4fv4n@users.noreply.github.com>
2019-11-21 12:18:55 +0530

    update url-extractor.sh

--- url-extractor.sh
+++ url-extractor.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-while read lnk ; do 
-	ruby /opt/relative-url-extractor/extract.rb $lnk
-	python /opt/LinkFinder/linkfinder.py -i $lnk -o cli
+if [ -z "$1" ]
+  then
+    echo "No file specified"
+    echo "Usage : ./url-extractor.sh filename"
+    exit 1
+else
+	while read lnk ; do 
+		ruby /opt/relative-url-extractor/extract.rb $lnk
+		python /opt/LinkFinder/linkfinder.py -i $lnk -o cli
 
-done < $1 > new_urls.txt
-
+	done < $1 > new_urls.txt
+fi
 
